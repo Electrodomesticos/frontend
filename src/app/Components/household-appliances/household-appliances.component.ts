d56ec484002a89a31e7c1843cae3be5ba3f9858d
Camilo Peña <capenap@unal.edu.co>
2017-10-05 01:28:09 -0500

    Botones editar y eliminar Electrodoméstico

--- src/app/Components/household-appliances/household-appliances.component.ts
+++ src/app/Components/household-appliances/household-appliances.component.ts
@@ -28,11 +28,37 @@ export class HouseholdAppliancesComponent implements OnInit {
         )
 }
 
+guardarDatos(appliance){
+  console.log("guardarDatos", appliance)
+  this.tempData=appliance;
+
+}
+
+updateAppliance(appliance){
+  console.log("Antes", this.tempData)
+  this.tempData.name=appliance.name;
+  this.tempData.electricity_use=appliance.electricity_use;
+  console.log("Despues",this.tempData)
+  this.appliancesService.updateAppliance(this.tempData).subscribe(
+    data => console.log('espacio para un alert', data),
+    error => console.error('espacio para un alert fallido'), ()=>this.loadAppliances());
+}
+
 createAppliance(appliance: Household_appliance) {
   this.appliancesService.setAppliance(appliance).subscribe(
     data => console.log('espacio para un alert', data),
     error => console.error('espacio para un alert fallido'), ()=>this.loadAppliances());
   
+}
+
+deleteAppliance(appliance) {
+  this.appliancesService.deleteAppliance(appliance.id)
+  .subscribe(
+    data => {'espacio para un alert'},
+    //error => {this.modal.open()},
+    error => {'espacio para un alert fallido'},
+    ()=>this.loadAppliances()
+    ); 
 }
 
   ngOnInit() {
